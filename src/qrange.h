a85299ec8d2a2dbb1106aaeb099a3de770493491
vt4a2h <vt4a2h@gmail.com>
2019-07-25 20:33:58 +0200

    Tree traversing algorithm depends on coroutines availability

--- src/qrange.h
+++ src/qrange.h
@@ -1,7 +1,12 @@
 #ifndef QRANGE_H
 #define QRANGE_H
 
-#include <range/v3/view_facade.hpp>
+#ifdef COROUTINES_SUPPORT
+#include <qrangecoro_p.h>
+#else
+#include <qrangefallback_p.h>
+#endif // COROUTINES_SUPPORT
+
 #include <range/v3/view/filter.hpp>
 #include <range/v3/view/transform.hpp>
 
@@ -13,71 +18,9 @@ namespace qt
     namespace r = ranges;
     namespace v = r::view;
 
-    namespace detail
-    {
-        struct cursor
-        {
-            std::shared_ptr<QObjectList> children;
-            int current_index = 0;
+    constexpr auto children = ranges::make_pipeable([](auto &&o) { return detail::children_view(o); });
 
-            decltype(auto) read() const
-            {
-                return (*children)[current_index];
-            }
-
-            void next()
-            {
-                ++current_index;
-            }
-
-            auto equal(ranges::default_sentinel_t) const
-            {
-                return current_index == children->count();
-            }
-
-            auto equal(const cursor &that) const
-            {
-                return current_index == that.current_index;
-            }
-
-            explicit cursor(const QObject *o, Qt::FindChildOptions opts)
-                : children(std::make_shared<QObjectList>(
-                               o ? o->findChildren<QObject*>(QString(), opts) : QObjectList()))
-            {}
-
-            cursor() = default;
-        };
-
-        template <class T = QObject>
-        class children_view : public ranges::view_facade<children_view<T>>
-        {
-            friend ranges::range_access;
-
-            T *obj;
-            Qt::FindChildOptions opts;
-
-            cursor begin_cursor()
-            {
-                return cursor(obj, opts);
-            }
-
-        public:
-            children_view() = default;
-            explicit children_view(T *o, Qt::FindChildOptions opts = Qt::FindChildrenRecursively)
-                : obj(o)
-                , opts(opts)
-            {}
-
-            explicit children_view(T &o, Qt::FindChildOptions opts = Qt::FindChildrenRecursively)
-                : children_view(&o, opts)
-            {}
-        };
-
-    }
-
-    const auto children = ranges::make_pipeable([](auto &&o) { return detail::children_view(o); });
-
-    auto find_children(Qt::FindChildOptions opts = Qt::FindChildrenRecursively)
+    constexpr auto find_children(Qt::FindChildOptions opts = Qt::FindChildrenRecursively)
     {
         return ranges::make_pipeable([opts](auto &&o) { return detail::children_view(o, opts); });
     }
